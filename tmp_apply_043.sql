b7f58d34a87bd0ded969760fdf452f5e3b8a863f
rian-d-moreira-opl <rian.moreira.ext@fibrasil.com.br>
2026-08-11 11:44:36 -0300

    fix: use text array type for statements in migration 043 patch

--- tmp_apply_043.sql
+++ tmp_apply_043.sql
@@ -39,6 +39,6 @@ INSERT INTO supabase_migrations.schema_migrations(version, name, statements)
 VALUES (
   '043',
   'voting_reminders_buckets_fixos',
-  ''
+  ARRAY[]::text[]
 )
 ON CONFLICT (version) DO NOTHING;
